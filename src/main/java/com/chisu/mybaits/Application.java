2605af504dcc3e8f51f63f85af0b5c6224924b14
chisu <chisu@myweimai.com>
2019-05-30 14:17:49 +0800

    Initial commit

---
+++ src/main/java/com/chisu/mybaits/Application.java
@@ -0,0 +1,13 @@
+package com.chisu.mybaits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
